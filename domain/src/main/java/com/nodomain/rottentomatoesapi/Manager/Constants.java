bf81ddf110062bb70b54d32aa39efbe5de5c0436
Florescu George Cătălin <Cataalinn@users.noreply.github.com>
2016-02-19 11:10:03 +0200

    Update Constants.java

--- domain/src/main/java/com/nodomain/rottentomatoesapi/Manager/Constants.java
+++ domain/src/main/java/com/nodomain/rottentomatoesapi/Manager/Constants.java
@@ -5,7 +5,7 @@ package com.nodomain.rottentomatoesapi.Manager;
  * Smart Mirror project
  */
 public class Constants {
-    public static final String API_KEY = "xbnkg2q8z26sbxcam6v874cv";
+    public static final String API_KEY = "your_api_key";
     public static final long SERVER_TIME_OUT = 30;
     public static final String SERVER_BASE_URL = "http://api.rottentomatoes.com";
 }
